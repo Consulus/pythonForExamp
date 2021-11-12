dc7d6fddb27acf7b758e40935fea0e36dac58769
Sergey Tumanov <tumanovser@mail.ru>
2021-11-12 14:22:58 +0300

    119 complete

---
+++ 119.py
@@ -0,0 +1,59 @@
+def ran():
+    import random
+    
+    small = input("Please Enter the small number: ")
+    
+    while small.isdigit() != True:
+        small = input("Please Enter the small number: ")
+    
+    small = int(small)    
+    
+    big = input("Please Enter the big number: ")
+    
+    while big.isdigit() != True:
+        big = input("Please Enter the big number: ")
+    
+    big = int(big)
+
+    comp_num = random.randint(small, big)
+    
+    return comp_num    
+
+def think():
+    
+    print("I am thinking of a number...")
+    answer = input("What number i think?")
+    
+    while answer.isdigit() != True:
+        answer = input("What number i think?")
+    
+    answer = int(answer)
+    
+    return answer
+    
+def game():
+
+    randomNumber = ran()
+    choise = think()
+    
+    while True:
+        
+        if randomNumber == choise:
+            print("Correct, you are win. The coorect number - " + str(choise))
+            return
+        
+        if randomNumber > choise:
+            print("The input number is low then correct")
+            choise = think()
+            continue
+        
+        if randomNumber < choise:
+            print("The input number is greater then correct")
+            choise = think()
+            continue
+
+game()                
+    
+        
+
+    

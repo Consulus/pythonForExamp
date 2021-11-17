4a0745f2826d283409ec35f39c7a8d3f22ec00c1
Tumanov Sergey <tumanovser@mail.ru>
2021-11-17 07:32:05 +0300

    start work at 121

---
+++ 121.py
@@ -0,0 +1,16 @@
+def show():
+    print("im show menu")
+
+def add():
+    print("im add menu")
+
+def change():
+    print("im change menu")
+
+def delete():
+    print("im delete menu")
+
+show()
+add()
+change()
+delete()

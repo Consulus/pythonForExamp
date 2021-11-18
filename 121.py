a99e25cdd37cf960c6742d330ee3adc8f884106d
Sergey Tumanov <tumanovser@mail.ru>
2021-11-18 15:34:53 +0300

    try to complete 121

--- 121.py
+++ 121.py
@@ -1,16 +1,57 @@
-def show():
-    print("im show menu")
+import csv
+import os
 
-def add():
-    print("im add menu")
+if os.path.exists("./Menu.csv") and os.path.getsize("./Menu.csv") > 0:
+    print("File exist and no empty")
+elif os.path.exists("./Menu.csv") == False:
+    print("File not exist")
+elif os.path.exists("./Menu.csv") and os.path.getsize("./Menu.csv") == 0:
+    print("File exist and empty")    
 
-def change():
-    print("im change menu")
+# file = open("Menu.csv", "r")
+# reader = csv.reader(file)
+# print(len(reader))
 
-def delete():
-    print("im delete menu")
+# if len(rows) == 0:
+    # file.close()
+    
+    # file = open("Menu.csv", "w")
+    # reader = csv.reader(file)
+    # rows = list(reader)
+    
+    # check = input("""
+        # Please check what need to do:
+            # 1 - Add name
+            # 2 - Change name
+            # 3 - Delete name
+            # 4 - Print names
+            # 5 - Exit programs
+        # """)
+    
+    # while check.isdigit() == False:
+        # check = input("""
+        # Please check number:
+            # 1 - Add name
+            # 2 - Change name
+            # 3 - Delete name
+            # 4 - Print names
+            # 5 - Exit programs
+        # """)
+    
+    # check = int(check)
+    
+    # print([1,2,3,4,5].index(check) )
+    
+# file.close()
 
-show()
-add()
-change()
-delete()
+# def show(lis):
+    # print(lis)
+
+# def add():
+    # print("im add menu")
+
+# def change():
+    # print("im change menu")
+
+# def delete():
+    # print("im delete menu")

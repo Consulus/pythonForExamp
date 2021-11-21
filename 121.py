9253170c62fba4a99d1ff8d1ebb6f46dd3a70aad
Tumanov Sergey <tumanovser@mail.ru>
2021-11-21 18:09:55 +0300

    complete 121 task. Im happy

--- 121.py
+++ 121.py
@@ -11,10 +11,21 @@ elif os.path.exists("./Menu.csv") and os.path.getsize("./Menu.csv") == 0:
 
 def showList():
     file = open("Menu.csv", "r")
+    for row in file:
+        print(row)
+
+def add():
+    file = open("Menu.csv", "r")
     reader = csv.reader(file)
     rows = list(reader)
-    for i in rows:
-        print(i)
+    num = len(rows)
+
+    file = open("Menu.csv", "a")
+    name = input("name: ")
+    file.write(str(num) + ") " + name + "\n")
+    file.close()
+
+
 
 def intro():
     check = input("""
@@ -36,108 +47,87 @@ def intro():
             5 - Exit programs
         """)    
 
+    while int(check) < 1 or int(check) > 5:
+        check = input("""
+        Please enter number in range 1 - 5:
+            1 - Add name
+            2 - Change name
+            3 - Delete name
+            4 - Print names
+            5 - Exit programs
+        """)  
+
     return int(check)
-
-
-
-
-        
-
-def add():
-    file = open("Menu.csv", "a")
-    name = input("Enter name: ")
-    newRecord = name + "\n"
-    file.write(str(newRecord))
-    file.close()
 
 def change():
     file = open("Menu.csv", "r")
-    reader = csv.reader(file)
-    rows = list(reader)
-    file.close()
-
+    arr = []
+    for i in file:
+        start = i.index(' ')
+        end = i.index('\n')
+        arr.append(i[start + 1:end])
+    
+    nameToChange = input("What name need to change: ")
+    while nameToChange not in arr:
+        showList()
+        nameToChange = input("Please enter the correct name need to change: ")
+    newName = input("Please enter new name: ")
+    index = arr.index(nameToChange)
+    arr[index] = newName
+    
     file = open("Menu.csv", "w")
+    for name in arr:
+        newRecord = str(arr.index(name)) + ') ' + name + "\n" 
+        file.write(str(newRecord))
+    file.close()
 
-    name = input("Please enter the name to change: ")
-    while name not in rows:
-        name = input("Name not in list, please enter another name: ")
-
-    for i in rows:
-        if i == name:
-            file.write(str(name) + "\n")
-        else:
-            file.write(str(i) + "\n")
+def deleteName():
+    file = open("Menu.csv", "r")
+    arr = []
+    for i in file:
+        start = i.index(' ')
+        end = i.index('\n')
+        arr.append(i[start + 1:end])
+    
+    nameToChange = input("What name need to delete: ")
+    while nameToChange not in arr:
+        showList()
+        nameToChange = input("Please enter the correct name need to delete: ")
+    arr.remove(nameToChange)
+    
+    file = open("Menu.csv", "w")
+    for name in arr:
+        newRecord = str(arr.index(name)) + ') ' + name + "\n" 
+        file.write(str(newRecord))
     file.close()
 
 
-value = 0
 
-
-
-while value != 5:
-    value = intro()
-    while value < 1 or value > 5:
-        print("Please enter number in range 1 - 5")
+def main():
+    value = 0 
+    while value != 5:
         value = intro()
 
-    if value == 1:
-        add()
-    if value == 2:
-        change()
-    if value == 3:
-        print("aa")
-    if value == 4:
-        showList()
-    if value == 5:
-        print("Programm close")            
+        if value == 5:
+            break
 
+        if value == 1:
+            add()
+            showList()
+            continue
 
+        if value == 2:
+            change()
+            showList()
+            continue
 
+        if value == 3:
+            deleteName()
+            showList()
+            continue
+        
+        if value == 4:
+            showList()
+            continue
 
-
-
-def delete():
-    print("im delete menu")
-
-
-
-
-
-
-# file = open("Menu.csv", "r")
-# reader = csv.reader(file)
-# print(len(reader))
-
-# if len(rows) == 0:
-#     file.close()
-    
-#     file = open("Menu.csv", "w")
-#     reader = csv.reader(file)
-#     rows = list(reader)
-    
-#     check = input("""
-#         Please check what need to do:
-#             1 - Add name
-#             2 - Change name
-#             3 - Delete name
-#             4 - Print names
-#             5 - Exit programs
-#         """)
-    
-#     while check.isdigit() == False:
-#         check = input("""
-#         Please check number:
-#             1 - Add name
-#             2 - Change name
-#             3 - Delete name
-#             4 - Print names
-#             5 - Exit programs
-#         """)
-    
-#     check = int(check)
-    
-#     print([1,2,3,4,5].index(check) )
-    
-# file.close()
-
-
+main()
